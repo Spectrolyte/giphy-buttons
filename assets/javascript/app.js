3ffc44b15bc6fdc1b8d422f1997552f04685ea95
Diana <dianaltecson@gmail.com>
2017-11-04 14:13:00 -0700

    added pseudocode to html and js files

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+// display default buttons
+	// default buttons are search terms as an array of strings
+	// create loop to iterate over entire array and display buttons in button section
+// when button is clicked:
+	// populate gifs for that search term in the gifs section with rating
+		// ajax call
+		// append new div containing rating and gif to gif section
+	// when a new button is clicked, replace the existing gifs with the new ones
+		// ajax call
+		// empty gif section
+		// append new div containing new gif content
+// user form
+	// when user submits form:
+		// capture input value and add to search term array
+		// empty button section
+		// loop over search term array to recreate buttons
